fb91de4ba1b89c8fcc1b16e302d8f2f5f954f6ab
Lorenzo Benevento <lorenzo.benevento@ddevs.io>
2024-10-14 15:38:18 +0200

    First commit

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ export { auth as middleware } from "auth"
 
 // Or like this if you need to do something here.
 // export default auth((req) => {
-//   console.log(req.auth) //  { session: { user: { ... } } }
+//   console.log(req) //  { session: { user: { ... } } }
 // })
 
 // Read more: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
